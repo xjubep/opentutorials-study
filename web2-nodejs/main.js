be3c7ef1856b7b47d9d4a47e3de37658042d6cd3
acergery <49668279+acergery@users.noreply.github.com>
2020-02-28 21:46:42 +0900

    v10: lec30 finish

--- web2-nodejs/main.js
+++ web2-nodejs/main.js
@@ -1,16 +1,87 @@
 var http = require('http');
 var fs = require('fs');
-var app = http.createServer(function(request, response) {
-    var url = request.url;
-    if (request.url == '/') {
-        url = '/index.html';
+var url = require('url');
+
+function templateHTML(title, list, description) {
+    return `
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>WEB - ${title}</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="colors.js"></script>
+    </head>
+
+    <body>
+        <h1><a href="/">WEB</a></h1>
+        <div id="grid">
+            ${list}
+            
+            <div id="article">
+                <h2>${title}</h2>
+                ${description}                
+            </div>
+        </div>
+        <div id="toggle">
+            <input type="button" value="night" onclick="nightDayHandler(this);">
+        </div>
+    </body>
+
+    </html>
+    `;
+}
+
+function templateList(title, filelist) {
+    var list = '<ol>';
+    for (var i = 0; i < filelist.length; i++) {
+        if (title === filelist[i]) {
+            list += '<b>'
+        }
+        list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+        if (title === filelist[i]) {
+            list += '</b>'
+        }
     }
-    if (request.url == '/favicon.ico') {
+    list += '</ol>';
+    return list;
+}
+
+var app = http.createServer(function (request, response) {
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+            fs.readdir('./data', function (error, filelist) {
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = templateList(title, filelist);
+                var template = templateHTML(title, list, description);
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+        else {
+            fs.readdir('./data', function (error, filelist) {
+                var title = queryData.id;
+                var list = templateList(title, filelist);
+
+                fs.readFile(`data/${title}`, 'utf8', function (err, description) {
+                    var template = templateHTML(title, list, description);
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            });
+        }
+    }
+    else {
         response.writeHead(404);
-        response.end();
-        return;
+        response.end('Not found');
     }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
 });
+
 app.listen(3000);
